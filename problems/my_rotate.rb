25650a970799ae1e2fd7ec216a118458b0735737
Ben Yu <intenex@gmail.com>
2019-03-16 16:40:31 -0700

    fixes specs for my_rotate and my_zip and adds instructions to my_rotate

--- problems/my_rotate.rb
+++ problems/my_rotate.rb
@@ -1,3 +1,10 @@
+# === 5 specs ===
+# The behavior should be identical to Array#rotate
+# When no argument is passed in, the method should default to rotating by 1.
+# Rotation shifts each index to the left 'positions' # of times
+# Example: [1, 2, 3, 4].my_rotate => [2, 3, 4, 1]
+# [1, 2, 3, 4].my_rotate(3) => [4, 1, 2, 3]
+
 class Array
 
   def my_rotate(positions)
