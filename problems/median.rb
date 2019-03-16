3c512c4ccc3a0d61e3fe68960f92859eab389913
Ben Yu <intenex@gmail.com>
2019-03-16 15:33:04 -0700

    Updates instructions for median to be more clear

--- problems/median.rb
+++ problems/median.rb
@@ -1,5 +1,8 @@
 # Write a method that returns the median of elements in an array
 # If the length is even, return the average of the middle two elements
+# Note that given elements in an array may not already be sorted.
+# Example: [3, 2, 6, 7] => 4.5
+# [3, 2, 6, 7, 1] => 3
 class Array
   def median
   end
