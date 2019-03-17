614127729eb916bb74e6cbea8643521a7dbcd3bb
Ben Yu <intenex@gmail.com>
2019-03-17 00:19:34 -0700

    Adds an optional memoized fibs_sum solution

--- solutions/fibs_sum_solution.rb
+++ solutions/fibs_sum_solution.rb
@@ -7,3 +7,16 @@ def fibs_sum(n)
 
   fibs_sum(n-1) + fibs_sum(n-2) + 1
 end
+
+# extra credit, a fully memoized solution that hackily gets around the recursive call spec
+# def fibs_sum(n, orig_n = nil)
+#   orig_n ||= n
+#   if n == 1
+#     return (orig_n == n ? 1 : [1])
+#   elsif n == 2
+#     return (orig_n == n ? 2 : [1, 1])
+#   end
+#   last_fib_seq = fibs_sum(n-1, orig_n)
+#   full_fib_seq = last_fib_seq + [last_fib_seq[-1] + last_fib_seq[-2]]
+#   orig_n == n ? full_fib_seq.sum : full_fib_seq
+# end
