863edae3df65a656dd6d24d428a758b88834f76c
Ben Yu <intenex@gmail.com>
2019-03-17 17:26:26 -0700

    remove the #my_any my_each spec

--- specs/my_any_spec.rb
+++ specs/my_any_spec.rb
@@ -1,13 +1,13 @@
 describe 'my_any' do
   arr = [1,2,3]
 
-  it "should use #my_each, NOT built-in ruby method" do
-    expect(arr).to receive(:my_each).and_call_original
-    expect(arr).not_to receive(:each)
-    expect(arr).not_to receive(:map)
-    expect(arr).not_to receive(:map!)
-    arr.my_any? { |num| num > 1 }
-  end
+  # it "should use #my_each, NOT built-in ruby method" do
+  #   expect(arr).to receive(:my_each).and_call_original
+  #   expect(arr).not_to receive(:each)
+  #   expect(arr).not_to receive(:map)
+  #   expect(arr).not_to receive(:map!)
+  #   arr.my_any? { |num| num > 1 }
+  # end
 
   it "returns true if any number matches the block" do
     expect(arr.my_any? { |num| num > 2 }).to eq(true)
